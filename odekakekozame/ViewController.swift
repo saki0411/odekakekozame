b345d02ea962b2f87bd97bc3beb5a0032b3f1601
sakihosonuma <chipdale411@gmail.com>
2023-02-24 10:10:49 +0900

    Initial Commit

---
+++ odekakekozame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odekakekozame
+//
+//  Created by saki on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
